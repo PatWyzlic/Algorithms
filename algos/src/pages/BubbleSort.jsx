987b25383073748b4c4643b00215d01e977c9b1f
patwyzlic <patrickwyzlic@gmail.com>
2022-08-11 13:33:25 -0400

    Format

--- algos/src/pages/BubbleSort.jsx
+++ algos/src/pages/BubbleSort.jsx
@@ -1,10 +1,16 @@
 
 
 export default function BubbleSort(){
-    
+    var arr = [234, 43, 55, 63,  5, 6, 235, 547];
+
     return(
-        <div>
+        <div class="main">
             <h1>Bubble Sort</h1>
+
+            
+
+
+
         </div>
     )
 }
