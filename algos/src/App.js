7ce855a4f8d35216673da68404977004c7de089c
patwyzlic <patrickwyzlic@gmail.com>
2022-08-12 16:37:50 -0400

    Changing default path to HomePage

--- algos/src/App.js
+++ algos/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import {Routes, Route} from "react-router-dom"
 import BubbleSort from './pages/BubbleSort';
 import ContainsDuplicate from './pages/ContainsDuplicate';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<BubbleSort/>}/>
+        <Route path="/" element={<HomePage/>}/>
         <Route path="/containsduplicate" element={<ContainsDuplicate/>}/>
       </Routes>
     </div>
