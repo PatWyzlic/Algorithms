1f0d16d0f025466179a22c66dd0a38d4e7f6c464
patwyzlic <patrickwyzlic@gmail.com>
2022-08-11 23:48:47 -0400

    Path Made for ContainsDuplicate

--- algos/src/App.js
+++ algos/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom"
 import BubbleSort from './pages/BubbleSort';
+import ContainsDuplicate from './pages/ContainsDuplicate';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<BubbleSort/>}/>
+        <Route path="/containsduplicate" element={<ContainsDuplicate/>}/>
       </Routes>
     </div>
   );
